9d1d9ca0741de7d5bc875c1c0623c6a70732275c
Fannar Snær <fannar@lambagras.is>
2016-10-04 13:43:07 +0000

    Overwriting worker config  with an local file
    Pointing the environment variable CONFIG_FILE to a json-file will
    trigger it to be read, parsed and added to the config, overwriting
    properties that are already present.

--- base-config.js
+++ base-config.js
@@ -1,20 +1,39 @@
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
+var xtend = require('xtend');
 var internalIp = require('internal-ip');
 var pkg = require(path.dirname(require.main.filename) + '/package.json');
 
 var pkgConfig = pkg.config || {};
 var config = {
-  name: process.env.SERVICE_NAME || pkg.name,
+  name: pkg.name,
   version: pkg.version,
   port: pkgConfig.port,
   services: pkgConfig.services,
-  environment: process.env.NODE_ENV || 'development',
-  consul: process.env.CONSUL || process.env.LOCAL_CONSUL || false,
+  environment: 'development',
+  consul: false,
   ip_address: internalIp.v4(),
-  log_level: process.env.LOG_LEVEL || 'info'
+  log_level: 'info'
 };
+
+if (process.env.CONFIG_FILE) {
+  var localConfig;
+  try {
+    localConfig = fs.readFileSync(process.env.CONFIG_FILE, 'utf8');
+    localConfig = JSON.parse(localConfig);
+  } catch (e) {
+    throw e;
+  }
+  console.log('[magistrate] Using local configuration file');
+  config = xtend(config, localConfig);
+}
+
+if (process.env.SERVICE_NAME) config.name = process.env.SERVICE_NAME;
+if (process.env.NODE_ENV) config.environment = process.env.NODE_ENV;
+if (process.env.CONSUL || process.env.LOCAL_CONSUL) config.consul = true;
+if (process.env.LOG_LEVEL) config.log_level = process.env.LOG_LEVEL;
 
 console.log('[magistrate] Base configuration is:', config);
 module.exports = config;
